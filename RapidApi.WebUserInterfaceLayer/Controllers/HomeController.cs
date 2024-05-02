6e9039f5c60e99ba7605ba193402b72ac66dec28
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-05-02 08:54:09 -0500

    Template added.

--- RapidApi.WebUserInterfaceLayer/Controllers/HomeController.cs
+++ RapidApi.WebUserInterfaceLayer/Controllers/HomeController.cs
@@ -5,28 +5,10 @@ using System.Diagnostics;
 namespace RapidApi.WebUserInterfaceLayer.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
+    {  
+        public async Task<IActionResult> Index()
+        { 
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        }        
     }
 }
